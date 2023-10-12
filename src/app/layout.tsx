db9a6c6ea13ffbe7441dd688d81efc09413e126c
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-10-12 11:47:13 +0330

    Tailwindcss stup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
-import { cn } from "@/lib/utils";
-import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+import { type Metadata } from "next";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
